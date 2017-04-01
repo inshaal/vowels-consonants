a62588cafcd1b7fb53779a351067182836b73bb9
Inshaal Uddin <inshaal@users.noreply.github.com>
2017-04-01 09:32:16 +0530

    code updated

--- vowelsandconsonants.py
+++ vowelsandconsonants.py
@@ -1,11 +1,12 @@
-#To find the number of vowels and consonants in the string
+#To find the number of vowels and consonants present in the string provided by the user.
 s=raw_input("Enter String : ")
-v,x=0,0
-for i in range (0,len(s)):
+d=len(s)
+v=0
+x=0
+for i in range (0,d):
     if s[i] in ['a','e','i','o','u']:
         v=v+1
     else:
         x=x+1
 print "The total no. of vowels are: ",v
 print "Consonants are: ",x
-print "                                         -by Ins"
